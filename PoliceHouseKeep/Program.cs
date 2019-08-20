ac81e9fa313132585682224efae533eb62491f07
janksand <sanyika717022@gmail.com>
2019-08-20 20:18:33 +0200

    Add project files.

---
+++ PoliceHouseKeep/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace PoliceHouseKeep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = File.ReadAllText("map.csv");
+            string[] result = text.Split(';', StringSplitOptions.RemoveEmptyEntries);
+            foreach (string item in result)
+            {
+                string[] res = item.Split(',');
+                foreach (string res1 in res)
+                {
+                    Console.Write($"{res1}\t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
